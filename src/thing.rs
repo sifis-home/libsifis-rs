2c211e1a6870f29d4bc9bea3a8a78944054c1516
Luni-4 <luni-4@hotmail.it>
2022-01-19 15:47:00 +0100

    Update thing deserializing

--- src/thing.rs
+++ src/thing.rs
@@ -4,80 +4,83 @@ use serde::Deserialize;
 
 use crate::label::Label;
 
-/*
 #[derive(Clone, Deserialize, Debug)]
-pub enum Op {
-    ReadProperty,
-    WriteProperty,
-    ObserveProperty,
-    UnobserveProperty,
-    InvokeAction,
-    SubscribeEvent,
-    UnsubscribeEvent,
-    ReadAllProperties,
-    WriteAllProperties,
-    ReadMultipleProperties,
-    WriteMultipleProperties,
+pub struct DataSchema {
+    pub maximum: Option<usize>,
+    pub minimum: Option<usize>,
+    pub title: Option<String>,
+    pub r#type: Option<String>,
+    pub unit: Option<String>,
 }
-
-#[derive(Clone, Deserialize, Debug)]
-pub enum SubProtocol {
-    Longpoll,
-    Websub,
-    Sse,
-}
-
-#[derive(Clone, Deserialize, Debug)]
-pub struct ExpectedResponse {
-    contentType: String,
-}
-
-#[derive(Clone, Deserialize, Debug)]
-pub struct Form {
-    op: Vec<Op>,
-    href: String,
-    contentType: String,
-    contentCoding: Option<String>,
-    subprotocol: Option<SubProtocol>,
-    security: Option<Vec<String>>,
-    scopes: Option<Vec<String>>,
-    response: Option<ExpectedResponse>,
-}
-*/
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct Affordance {
-    #[serde(default, rename = "sifis:hazard")]
-    hazard: serde_json::Value,
-    // title: String,
-    // description: String,
-    //    forms: Vec<Form>,
+    /*#[serde(default, rename = "sifis:hazard")]
+    hazard: serde_json::Value,*/
+    #[serde(rename = "@type")]
+    pub attype: Option<String>,
+    pub description: Option<String>,
+    #[serde(rename = "links")]
+    pub forms: Vec<Form>,
+    pub titles: Option<HashMap<String, String>>,
+    pub descriptions: Option<HashMap<String, String>>,
 }
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct Property {
     #[serde(flatten)]
-    inner: Affordance,
+    pub affordance: Affordance,
+    pub observable: Option<bool>,
 }
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct Action {
     #[serde(flatten)]
-    inner: Affordance,
+    pub affordance: Affordance,
+    pub input: Option<DataSchema>,
+    pub output: Option<DataSchema>,
+    #[serde(default)]
+    pub safe: bool,
+    #[serde(default)]
+    pub idempotent: bool,
 }
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct Event {
     #[serde(flatten)]
-    inner: Affordance,
+    pub affordance: Affordance,
+    pub subscription: Option<DataSchema>,
+    pub data: Option<DataSchema>,
+    pub cancellation: Option<DataSchema>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct SecurityScheme {
+    #[serde(flatten)]
+    pub scheme: HashMap<String, String>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Form {
+    pub href: String,
+    pub rel: String,
 }
 
 /// Connected thing
 #[derive(Clone, Debug, Deserialize)]
 pub struct Thing {
+    #[serde(rename = "@context")]
+    pub context: String,
     pub title: String,
     #[serde(rename = "@type")]
     pub attype: Vec<String>,
+    pub description: String,
+    pub base: String,
+    pub security: String,
+    #[serde(rename = "securityDefinitions")]
+    pub security_definition: HashMap<String, SecurityScheme>,
+    #[serde(rename = "links")]
+    pub forms: Vec<Form>,
     properties: HashMap<String, Property>,
     actions: HashMap<String, Action>,
     events: HashMap<String, Event>,
