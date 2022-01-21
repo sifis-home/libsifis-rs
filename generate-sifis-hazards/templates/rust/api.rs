673821d0917a512026d4c8ed5808bfb522547874
Luni-4 <luni-4@hotmail.it>
2022-01-21 18:22:56 +0100

    Update ontology

--- generate-sifis-hazards/templates/rust/api.rs
+++ generate-sifis-hazards/templates/rust/api.rs
@@ -1,18 +1,18 @@
-/// Type of Hazards
+/// Hazards type.
 pub enum Hazards {
 {%- for hazard in hazards -%}
     {{ hazard }},
 {%- endfor -%}
 }
 
-/// {{ sho_FireHazard_name }}
-/// {{ sho_FireHazard_description }}
+/// {{ sho_FireHazard_name }}.
+/// {{ sho_FireHazard_description }}.
 pub fn turn_on_light() {
     todo!("Implement using the Wot API");
 }
 
-/// {{ sho_FireHazard_name }}
-/// {{ sho_FireHazard_description }}
+/// {{ sho_FireHazard_name }}.
+/// {{ sho_FireHazard_description }}.
 pub fn turn_on_oven() {
     todo!("Implement using the Wot API");
 }
--- sifis-abstraction/src/lib.rs
+++ sifis-abstraction/src/lib.rs
@@ -1,4 +1,4 @@
-/// Type of Hazards
+/// Hazards type.
 pub enum Hazards {
     AirPoisoning,
     Asphyxia,
@@ -27,14 +27,14 @@ pub enum Hazards {
     WaterFlooding,
 }
 
-/// fire hazard
-/// The execution may cause fire
+/// Fire hazard.
+/// The execution may cause fire.
 pub fn turn_on_light() {
     todo!("Implement using the Wot API");
 }
 
-/// fire hazard
-/// The execution may cause fire
+/// Fire hazard.
+/// The execution may cause fire.
 pub fn turn_on_oven() {
     todo!("Implement using the Wot API");
 }
